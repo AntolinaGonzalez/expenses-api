933fcec00f287d354ab85c4f19cf4ff0a36cbd86
AntolinaGonzalez <antolina487@gmail.com>
2023-09-26 14:30:28 -0300

    fix error

--- src/main/java/com/project/expensesapi/data/ExpenseDTO.java
+++ src/main/java/com/project/expensesapi/data/ExpenseDTO.java
@@ -2,10 +2,12 @@ package com.project.expensesapi.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExpenseDTO {
     @NotNull
     private ExpenseType type;
